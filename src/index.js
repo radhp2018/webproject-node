78e19ec16c4ae4af513bdab872a93b8e128f0e03
radhp2005@gmail.com <radhp2005@gmail.com>
2022-01-29 14:02:53 -0500

    Update porcess env and script tag file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 const staticPath = path.join(__dirname, "../public")
 //builtin middleware
 app.use(express.static(staticPath));
